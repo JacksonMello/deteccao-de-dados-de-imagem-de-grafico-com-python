bd4d66ff0f1e7095e0bd50c7771cbb8c3e388fce
JACKSON ALVES DA SILVA MELLO <92218199+JacksonMello@users.noreply.github.com>
2023-08-01 20:59:22 -0300

    Update functions.py

--- Python/functions.py
+++ Python/functions.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 img = cv.imread('thresh_tabela.png',0)
 
@@ -65,3 +66,52 @@ def refinar_curva(EIXO_X, CURVA):
             CURVA_REFINADA.append(pontos_Y[int(n//2)])
     
     return CURVA_REFINADA
+
+def get_tab_px(EIXO_X, CURVA_REFINADA):
+    
+    EIXO_X.pop(0) #extrai o ponto da origem (0, 0), pois a curva não possui correspondente para este pixel
+
+    TAB_PIXEIS = []
+    cont_px = 0
+    for pixel in EIXO_X:
+        
+        y = pixel[0] - CURVA_REFINADA[cont_px][0]
+        TAB_PIXEIS.append([cont_px + 1, y])
+
+        cont_px += 1
+
+    return TAB_PIXEIS
+
+def get_tab_final(TABELA_PX, ORIGEM_REAL, RANGE_REAL, RANGE_PX):
+
+    const_X = RANGE_REAL[0] / RANGE_PX[0] #o valor real de 1px em X
+    const_Y = RANGE_REAL[1] / RANGE_PX[1] #o valor real de 1px em Y
+
+    TAB_FINAL = []
+    for ponto in TABELA_PX:
+
+        X = ORIGEM_REAL[0] + const_X * ponto[0]
+        Y = ORIGEM_REAL[1] + const_Y * ponto[1]
+        TAB_FINAL.append([X,Y])
+    
+    return TAB_FINAL
+
+def gerar_grafico(DADOS, ORIGEM_REAL, RANGE_REAL):
+    
+    EIXOS = [  # [xmin, xmax, ymin, ymax]
+        ORIGEM_REAL[0],
+        ORIGEM_REAL[0] + RANGE_REAL[0],
+        ORIGEM_REAL[1],
+        ORIGEM_REAL[1] + RANGE_REAL[1]
+    ]
+    LIST_X = []
+    LIST_Y = []
+    for coord in DADOS:
+        LIST_X.append(coord[0])
+        LIST_Y.append(coord[1])
+    
+    plt.plot(LIST_X, LIST_Y)
+    plt.xlabel('Sut (MPa)')
+    plt.ylabel('f')
+    plt.axis(EIXOS)
+    plt.show()
