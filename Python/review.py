3a691629ac070d841812eb72561f282efc04c90f
JACKSON ALVES DA SILVA MELLO <92218199+JacksonMello@users.noreply.github.com>
2023-08-01 21:00:22 -0300

    Update review.py

--- Python/review.py
+++ Python/review.py
@@ -1,12 +1,28 @@
-from functions import get_pixeis, get_origem, get_componentes, refinar_curva
+from functions import *
+import json
 
 PIXEIS = get_pixeis()
-REFS = get_origem(PIXEIS)
+REFS = get_origem(PIXEIS) #retorna coordenadas na img de ORIGEM, min e max, respectivamente.
 
 [EIXO_X, EIXO_Y, CURVA] = get_componentes(PIXEIS, REFS[0]) #eixos X, eixo Y e a CURVA
 
-print(f'Eixo X: {len(EIXO_X)}')
-print(f'Eixo Y: {len(EIXO_Y)}')
+# print(f'Eixo X: {len(EIXO_X)}')
+# print(f'Eixo Y: {len(EIXO_Y)}')
 
 CURVA_REFINADA = refinar_curva(EIXO_X, CURVA)
-print(f'Total de pontos Final: {len(CURVA_REFINADA)}')
+TAB_PIXEIS = get_tab_px(EIXO_X, CURVA_REFINADA)
+
+h_0 = REFS[2][0] - REFS[1][0] #valor máximo na origem
+TAB_PIXEIS.insert(0, [0,h_0])
+
+ORIGEM_REAL = [490, 0.76]
+RANGE_REAL = [910, 0.14]
+RANGE_PX = [len(EIXO_X),len(EIXO_Y)] #acrecenta-se 1 ao eixo Y pois este pixel não foi incluido neste eixo na extração de componentes
+
+TAB_FINAL = get_tab_final(TAB_PIXEIS, ORIGEM_REAL, RANGE_REAL, RANGE_PX)
+
+arquivo = open('Coord_Grafico_f.json', 'w')
+json.dump(TAB_FINAL,arquivo)
+arquivo.close()
+
+gerar_grafico(TAB_FINAL, ORIGEM_REAL, RANGE_REAL)
