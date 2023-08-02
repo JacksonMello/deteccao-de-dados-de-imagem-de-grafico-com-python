c738d03a72d7f7497b1156ad146169fb9096b7c8
JACKSON ALVES DA SILVA MELLO <92218199+JacksonMello@users.noreply.github.com>
2023-08-01 21:51:04 -0300

    Update view.py

--- view.py
+++ view.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 
+#gerar gráfico em escala de cinza e nesta selecionar apenas os píxeis com intencidade até 160, os demais pixeis serão 255 (branco).
+
 img = cv.imread('tabela.png',cv.IMREAD_GRAYSCALE)
 ret,thresh = cv.threshold(img,160,255,cv.THRESH_BINARY)
-cv.imwrite('thresh_tabela.png',thresh)
+cv.imwrite('thresh_tabela.png',thresh) #Salvar imagem para coleta de pixeis
